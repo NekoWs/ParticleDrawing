ce6bb47959b748d53412cf55ad01526269ed78fd
NekoWs <1689295608@qq.com>
2026-08-11 00:15:59 +0800

    上传空的模组项目

---
+++ src/main/kotlin/work/nekow/particledrawing/ParticleDrawing.kt
@@ -0,0 +1,19 @@
+package work.nekow.particledrawing
+
+import com.mojang.logging.LogUtils
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.fml.ModContainer
+import net.neoforged.fml.common.Mod
+import org.slf4j.Logger
+
+@Mod(ParticleDrawing.MODID)
+class ParticleDrawing(modEventBus: IEventBus, modContainer: ModContainer) {
+    companion object {
+        const val MODID = "particledrawing"
+        private val LOGGER: Logger = LogUtils.getLogger()
+    }
+
+    init {
+        LOGGER.info("Loading ParticleDrawing!")
+    }
+}
