a4f8976aba8f86e334f4732cb7e510b78ba16cb3
NekoWs <1689295608@qq.com>
2026-08-11 07:24:58 +0800

    移除没用的代码和文件

--- src/main/kotlin/work/nekow/particledrawing/ParticleDrawing.kt
+++ src/main/kotlin/work/nekow/particledrawing/ParticleDrawing.kt
@@ -1,23 +1,18 @@
 package work.nekow.particledrawing
 
-import com.mojang.logging.LogUtils
 import net.neoforged.bus.api.IEventBus
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.config.ModConfig
-import org.slf4j.Logger
 import work.nekow.particledrawing.config.ParticleDrawingConfig
 
 @Mod(ParticleDrawing.MODID)
 class ParticleDrawing(bus: IEventBus, container: ModContainer) {
     companion object {
         const val MODID = "particledrawing"
-        private val LOGGER: Logger = LogUtils.getLogger()
     }
 
     init {
-        LOGGER.info("Loading ParticleDrawing!")
-
         container.registerConfig(ModConfig.Type.SERVER, ParticleDrawingConfig.SERVER_SPEC)
         container.registerConfig(ModConfig.Type.CLIENT, ParticleDrawingConfig.CLIENT_SPEC)
     }
